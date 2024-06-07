612ec2848faebcaa99ff489b7206e1514e2df2db
doudoua123 <zhmlrf@163.com>
2024-06-07 20:52:15 +0800

    Redux转态管理

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement, addToNum } from './store/modules/counterStore'
+import { fetchChannels } from './store/modules/channelStore'
+import { useEffect } from "react";
 
 function App() {
+  const { count } = useSelector(state => state.counter)
+  const { channelList } = useSelector(state => state.channle)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchChannels())
+  },[dispatch])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => dispatch(decrement())}>减</button>
+      {count}
+      <button onClick={() => dispatch(increment())}>增</button>
+      <button onClick={() => dispatch(addToNum(10))}>addTo10</button>
+      <button onClick={() => dispatch(addToNum(20))}>addTo20</button>
+      <ul>
+        {channelList.map((item) => <li key={item.id}>{item.name}</li>)}
+      </ul>
     </div>
   );
 }
